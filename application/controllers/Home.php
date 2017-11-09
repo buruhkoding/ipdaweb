4e7a6622cb06daa9ccb2442e642c86c2ee4cdf95
rahmatxfauzi <madfizh@gmail.com>
2017-11-09 20:14:53 +0700

    Clear All - IPDAv1.0

---
+++ application/controllers/Home.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+* 
+*/
+class Home extends CI_Controller
+{
+	
+	// function __construct(argument)
+	// {
+	// 	# code...
+	// }
+
+	function index() {
+		echo "heheh";
+	}
+}
---
+++ application/views/Home.php
@@ -0,0 +1,5 @@
+<html>
+<head>
+<title>IPDA - Antarejo Anggi</title>
+</head>
+</html>
